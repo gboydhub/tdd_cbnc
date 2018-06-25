708764eca7d6360f0a3680a20d042613dea36f11
Gary Boyd <eponick@gmail.com>
2018-06-25 11:03:11 -0400

    Match check returns number, fail

--- test_cbnc.rb
+++ test_cbnc.rb
@@ -5,4 +5,8 @@ class TestPizzaApp < Minitest::Test
     def test_assert_1_equals_1
         assert_equal(1, 1)
     end
+
+    def test_match_returns_boolean
+        assert_equal(Integer, match_check("","").class)
+    end
 end
